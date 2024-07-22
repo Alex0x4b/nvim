f8ea636c2a449751450d5be61a78a3ecfd7c77ac
Alex0x4b <76492657+Alex0x4b@users.noreply.github.com>
2024-07-22 11:12:24 +0200

    [nvim] -add custom remap for neoscroll

--- lua/settings/lazy/neoscroll.lua
+++ lua/settings/lazy/neoscroll.lua
@@ -1,9 +1,20 @@
 return {
-  "karb94/neoscroll.nvim",
-  config = function ()
-    require('neoscroll').setup({
-        easing = 'cubic'
-
-    })
-  end
+    "karb94/neoscroll.nvim",
+    config = function ()
+        require('neoscroll').setup({
+            easing = 'cubic',
+            cursor_scrolls_alone = false
+        })
+        local neoscroll = require('neoscroll')
+        local keymap = {
+            ["<C-u>"] = function() neoscroll.ctrl_u({ duration = 200 }) end;
+            ["<C-d>"] = function() neoscroll.ctrl_d({ duration = 200 }) end;
+            ["zz"]    = function() neoscroll.zt({ half_win_duration = 200 }) end;
+            ["zm"]    = function() neoscroll.zz({ half_win_duration = 200 }) end;
+        }
+        local modes = { 'n', 'v', 'x' }
+        for key, func in pairs(keymap) do
+            vim.keymap.set(modes, key, func)
+        end
+    end
 }
