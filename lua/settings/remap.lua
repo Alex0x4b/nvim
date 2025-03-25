d06420e800592e33d7ce45ca6a946b91d78228e2
Alex0x4b <76492657+Alex0x4b@users.noreply.github.com>
2025-03-25 15:55:13 +0100

    -add remap for buffer

--- lua/settings/remap.lua
+++ lua/settings/remap.lua
@@ -4,18 +4,26 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
-
--- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
+-- Buffer
+vim.keymap.set('n', '<C-j>', ':bnext<CR>', {
+    noremap = true,
+    silent = true,
+    desc = 'Go to next buffer'
+})
+vim.keymap.set('n', '<C-k>', ':bprevious<CR>', {
+    noremap = true,
+    silent = true,
+    desc = 'Go to previous buffer'
+})
